ca52a110c5472df2c7727d705c9a9ea6e8f33f66
Mykhailo Myrosh <mishamyrosh03@gmail.com>
2024-07-07 16:33:37 +0300

    Initial commit

---
+++ src/components/EnterPoint.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class Page extends React.Component {
+  render() {
+    return (
+      <div className="component-page">
+        <h1>Welcome to the Roman Banakh page</h1>
+        <p>If you want to use this app please <Link to="/register">register</Link></p>
+      </div>
+    );
+  }
+}
---
+++ src/components/LoginPage .js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import '../styles/LoginPage.css';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+    console.log('Login:', { email, password });
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <h2>Sign in</h2>
+        <p>Sign in to start monitoring what is going on around you</p>
+        <form onSubmit={handleLogin}>
+          <div className="input-group">
+            <input 
+              type="text" 
+              placeholder="Login" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required 
+            />
+          </div>
+          <div className="input-group">
+            <input 
+              type="password" 
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required 
+            />
+          </div>
+          <div className="options">
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="/forgot-password">Forgot password?</a>
+          </div>
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
